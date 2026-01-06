3317e2f1eb623a510620f6afbf963b07c86740d7
Ben Whitfield-Heap <benjaminpheap@gmail.com>
2026-01-06 19:49:07 +0000

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/louth-civic-2025/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
